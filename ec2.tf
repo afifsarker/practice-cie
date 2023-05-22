26f59b8a8a108635247d3a84fa6e5ad601aa4eb0
afifsarker <91082518+afifsarker@users.noreply.github.com>
2023-05-22 21:53:01 +0600

    Update ec2.tfd

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,4 @@
 resource "aws_instance" "web_server" {
   ami                    = "ami-0ff8a91507f77f867"  #for amazon linux
  instance_type          = "t2.micro"
-  tags = {
-    Name = "web"
-  }
 }
