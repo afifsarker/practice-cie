b78d11e946bc53b9d8654157f0aa62a59fa26589
afifsarker <91082518+afifsarker@users.noreply.github.com>
2023-05-22 23:02:51 +0600

    Update ec2.tf
    add tag

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "web_server" {
   ami                    = "ami-0ff8a91507f77f867"  #for amazon linux
  instance_type          = "t2.micro"
+  tags = {
+    Name = "nginx"
+  }
 }
